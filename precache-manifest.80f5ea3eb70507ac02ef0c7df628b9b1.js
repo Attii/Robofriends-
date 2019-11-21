d236f3c46cbbbeeb130108853a4f7be190ace2e8
Attii <attij@yandex.ru>
2019-11-20 22:22:37 -0500

    Updates

---
+++ precache-manifest.80f5ea3eb70507ac02ef0c7df628b9b1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c499b398e3b1c4b2f7bf9559e01b0ac5",
+    "url": "/Robofriends-/index.html"
+  },
+  {
+    "revision": "eb2e83d34c498388c016",
+    "url": "/Robofriends-/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "81850d6a692ed1b1b1e2",
+    "url": "/Robofriends-/static/css/main.2c5f10eb.chunk.css"
+  },
+  {
+    "revision": "eb2e83d34c498388c016",
+    "url": "/Robofriends-/static/js/2.591f2f3b.chunk.js"
+  },
+  {
+    "revision": "81850d6a692ed1b1b1e2",
+    "url": "/Robofriends-/static/js/main.a3499409.chunk.js"
+  },
+  {
+    "revision": "fedc7658c973fb926df0",
+    "url": "/Robofriends-/static/js/runtime-main.fca493f4.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/Robofriends-/static/media/SEGA.12a3db8b.woff"
+  }
+]);
